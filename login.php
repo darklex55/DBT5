24c60b0922b09468160f6542c0184f6c6774f703
Ilias Chanis <iliashanis@gmail.com>
2020-01-08 21:25:06 +0200

    AUTH: Removed HTTPS requirement.

--- login.php
+++ login.php
@@ -15,113 +15,106 @@ header("X-Content-Type-Options: nosniff");
 header("Content-Security-Policy: default-src 'self'; script-src 'self';");
 
 /* Login logic */
-// Enforce HTTPS only
-if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') {
-    header("Strict-Transport-Security: max-age=31536000; includeSubDomains; preload");
-    session_start();
+session_start();
 
-    // Hashing cookie name to try and avoid attacks by compromised
-    // sites that use the same cookie name and scope
-    $hashed_name = md5('auth_token_cookie');
-    // Check if the user is already logged in
-    if (isset($_COOKIE[$hashed_name]) && isset($_SESSION['user_id'])) {
-        header("Location: ../index.php");
-        die(0);
-    } else if(isset($_POST['email']) && isset($_POST['password'])){
-        $email = $_POST['email'];
-        $password = $_POST['password'];
+// Hashing cookie name to try and avoid attacks by compromised
+// sites that use the same cookie name and scope
+$hashed_name = md5('auth_token_cookie');
+// Check if the user is already logged in
+if (isset($_COOKIE[$hashed_name]) && isset($_SESSION['user_id'])) {
+    header("Location: ./index.php");
+    die(0);
+} else if(isset($_POST['email']) && isset($_POST['password'])){
+    $email = $_POST['email'];
+    $password = $_POST['password'];
 
-        // Confirm input is sanitized
-        if (filter_var($email, FILTER_VALIDATE_EMAIL) && (strlen($email) >= 10) && (strlen($email) <= 100) && (strlen($password) >= 8) && (strlen($password) <= 50)) {
-            require_once "./connect.php";
-            $dbconnect = new Connection();
-            $db = $dbconnect->openConnection();
+    // Confirm input is sanitized
+    if (filter_var($email, FILTER_VALIDATE_EMAIL) && (strlen($email) >= 10) && (strlen($email) <= 100) && (strlen($password) >= 8) && (strlen($password) <= 50)) {
+        require_once "./connect.php";
+        $dbconnect = new Connection();
+        $db = $dbconnect->openConnection();
 
-            $query = $db->prepare("SELECT `id`, `password`, `specialty`, `dept_clinic_id`, `name`, `surname`, `gender`, count(*) AS num_rows FROM `employees` WHERE `email` = :email");
-            $query->execute(['email' => $email]);
-            $result = $query->fetch(PDO::FETCH_ASSOC);
+        $query = $db->prepare("SELECT `id`, `password`, `specialty`, `dept_clinic_id`, `name`, `surname`, `gender`, count(*) AS num_rows FROM `employees` WHERE `email` = :email");
+        $query->execute(['email' => $email]);
+        $result = $query->fetch(PDO::FETCH_ASSOC);
 
-            $dbconnect->closeConnection();
+        $dbconnect->closeConnection();
 
-            $database_id = $result['id'];
-            $database_pass = $result['password'];
-            $database_specialty = $result['specialty'];
-            $database_clinic_id = $result['dept_clinic_id'];
-            $database_name = $result['name'] . " " . $result['surname'];
-            $database_gender = $result['gender'];
-            $num_of_rows = $result['num_rows'];
+        $database_id = $result['id'];
+        $database_pass = $result['password'];
+        $database_specialty = $result['specialty'];
+        $database_clinic_id = $result['dept_clinic_id'];
+        $database_name = $result['name'] . " " . $result['surname'];
+        $database_gender = $result['gender'];
+        $num_of_rows = $result['num_rows'];
 
-            // Checks if the user exists.
-            if ($num_of_rows == 1) {
-                // Checks the password recovered from the database
-                // against the one entered by the user.
-                if (password_verify($password, $database_pass)) {
-                    session_regenerate_id(true);
-                    $session_cookie_params = session_get_cookie_params();
+        // Checks if the user exists.
+        if ($num_of_rows == 1) {
+            // Checks the password recovered from the database
+            // against the one entered by the user.
+            if (password_verify($password, $database_pass)) {
+                session_regenerate_id(true);
+                $session_cookie_params = session_get_cookie_params();
 
-                    // Generate a token, set the authentication cookie
-                    // and save the cookie (valid for 12h).
-                    $token = bin2hex(openssl_random_pseudo_bytes(32));
-                    setcookie($hashed_name, // token name
-                        $token, // token value
-                        0, // expires at end of session
-                        $session_cookie_params['path'], // path
-                        $session_cookie_params['domain'], // domain
-                        true, // secure			CHANGE TO true
-                        true //http-only
-                    );
+                // Generate a token, set the authentication cookie
+                // and save the cookie (valid for 12h).
+                $token = bin2hex(openssl_random_pseudo_bytes(32));
+                setcookie($hashed_name, // token name
+                    $token, // token value
+                    0, // expires at end of session
+                    $session_cookie_params['path'], // path
+                    $session_cookie_params['domain'], // domain
+                    false, // secure			CHANGE TO true
+                    true //http-only
+                );
 
-                    $time = time();
-                    $valid_until = $time + 43200;
+                $time = time();
+                $valid_until = $time + 43200;
 
-                    $db = $dbconnect->openConnection();
+                $db = $dbconnect->openConnection();
 
-                    $query = $db->prepare("INSERT INTO `tokens` (`user_id`, `token`, `valid_until`, `last_use`) VALUES (:id, :token, :valid, :last_use)");
-                    $query->execute(['id' => $database_id,
-                                    'token' => $token,
-                                    'valid' => $valid_until,
-                                    'last_use' => $time]);
+                $query = $db->prepare("INSERT INTO `tokens` (`user_id`, `token`, `valid_until`, `last_use`) VALUES (:id, :token, :valid, :last_use)");
+                $query->execute(['id' => $database_id,
+                                'token' => $token,
+                                'valid' => $valid_until,
+                                'last_use' => $time]);
 
-                    $dbconnect->closeConnection();
+                $dbconnect->closeConnection();
 
-                    // Update session variables and redirect
-                    $_SESSION['user_id'] = $database_id;
-                    $_SESSION['clinic_id'] = $database_clinic_id;
-                    $_SESSION['name'] = $database_name;
-                    $_SESSION['gender'] = $database_gender;
-                    if($database_specialty == 'Nurse') {
-                        $_SESSION['access_level'] = 2;
-                    } else {
-                        $_SESSION['access_level'] = 1;
-                    }
-
-                    header("Location: ./index.php");
-                    die(0);
+                // Update session variables and redirect
+                $_SESSION['user_id'] = $database_id;
+                $_SESSION['clinic_id'] = $database_clinic_id;
+                $_SESSION['name'] = $database_name;
+                $_SESSION['gender'] = $database_gender;
+                if($database_specialty == 'Nurse') {
+                    $_SESSION['access_level'] = 2;
                 } else {
-                    // Login failed (wrong password)
-                    render_html(4);
-                    die(4);
+                    $_SESSION['access_level'] = 1;
                 }
+
+                header("Location: ./index.php");
+                die(0);
             } else {
-                // Login failed (user does not exist)
-                render_html(3);
-                die(3);
+                // Login failed (wrong password)
+                render_html(4);
+                die(4);
             }
         } else {
-            // Login failed (garbage input)
-            render_html(2);
-            die(2);
+            // Login failed (user does not exist)
+            render_html(3);
+            die(3);
         }
     } else {
-        // Form is not yet submited, POST request is not yet done
-        render_html(1);
-        die(1);
+        // Login failed (garbage input)
+        render_html(2);
+        die(2);
     }
 } else {
-    // Force HTTPS
-    header("Location: https://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'], true, 301);
-    die(0);
+    // Form is not yet submited, POST request is not yet done
+    render_html(1);
+    die(1);
 }
+
 
 function render_html($error_code) {
 ?>
